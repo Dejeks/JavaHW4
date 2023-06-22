a9f47ee379bd9050db4244e738dc68b441c6f7c9
Danil Neklyudov <dejeksdan@gmail.com>
2023-06-22 21:50:25 +0500

    Task2 comleted

---
+++ Task2.java
@@ -0,0 +1,41 @@
+package org.example.Homework.HW4;
+import java.util.Stack;
+
+public class Task2 {
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                if ((c == ')' && top != '(') ||
+                        (c == '}' && top != '{') ||
+                        (c == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String input1 = "(){}[]";
+        System.out.println(input1 + " is valid: " + isValid(input1));
+
+        String input2 = "([{}])";
+        System.out.println(input2 + " is valid: " + isValid(input2));
+
+        String input3 = "({[})";
+        System.out.println(input3 + " is valid: " + isValid(input3));
+
+        String input4 = "]";
+        System.out.println(input4 + " is valid: " + isValid(input4));
+
+        String input5 = "({}[]])";
+        System.out.println(input5 + " is valid: " + isValid(input5));
+    }
+}
